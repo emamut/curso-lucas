11f34b27967fa7ea2c5c5ac91b3337b3a2b85032
Faber Andrés Vergara Holguín <fabervergara@gmail.com>
2020-06-23 21:24:56 -0500

    start with events

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 var app = new Vue({
   el: '#app',
   data: {
-    questionsArray: []
+    questionsArray: [{}],
+    activeQuestion: 0,
+    selectAnswers: []
   },
   mounted: function () {
     var self = this
@@ -10,5 +12,15 @@ var app = new Vue({
       .then(function (response) {
         self.questionsArray = response.data
       })
+  },
+  methods: {
+    getSelectedAnswer: function (answer) {
+      this.selectAnswers.push(answer)
+
+      if(this.activeQuestion < (this.questionsArray.length - 1))
+        this.activeQuestion++
+      else
+        alert('es el final')
+    }
   }
 })
